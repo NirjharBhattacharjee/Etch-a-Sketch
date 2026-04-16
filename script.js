b9c429db02948a15738f757e7967d1ccc2aa1f7d
Nirjhar Bhattacharjee <bhattacharjeenirjhar26@gmail.com>
2026-04-16 21:06:14 +1000

    feat: create UI for the sketch book

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const container = document.querySelector(".container");
 
-function createGrid(cells) {
-    if(cells > 1){
-        for(let i = 0; i < cells; i++){
-            const column = document.createElement("div");
-            for (let j = 0; j < cells ; j++) {
-                const row = document.createElement("div");
-                row.classList="row-cell";
-                column.appendChild(row);
-            }
-            column.classList ="column-cell";
-            container.appendChild(column);
-    }
-   
-} 
-}
-createGrid(8);
+function createGrid(height =16, width =16) {
+ 
+    for(let i = 0; i < height; i++){
+        for (let j = 0; j < width ; j++) {
+            const cell = document.createElement("div");
+            cell.classList="cell";
+            let cellHeight = cell.style.height =`${512/height}px`;
+            let cellWidth = cell.style.width = `${512/width}px`;
+            console.log(`Cell Height : ${cellHeight}`);
+            console.log(`Cell Width : ${cellWidth}`);
+            container.appendChild(cell);
+        }
 
-// 
+};   
+};
+createGrid();
+
